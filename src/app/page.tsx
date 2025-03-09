ed474f34b2b80222ba27d7ce1cbd376d5dec93b3
adnanahid <sikuahamed@gmail.com>
2025-03-09 19:42:30 +0600

    final checking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Weather from "./components/Weather";
 export default function Home() {
   return (
     <div className="relative p-5 mx-auto">
-      <div className="w-48 h-48 sm:w-96 sm:h-96 rounded-full bg-gray-300 absolute -top-16 -left-16 sm:-top-[150px] sm:-left-[180px] z-0 shadow-[0px_0px_30px_10px_rgba(255,255,255,0.5)]"></div>
+      <div className="w-48 h-48 sm:w-96 sm:h-96 rounded-full bg-gray-300 absolute -top-20 -left-16 sm:-top-[150px] sm:-left-[180px] z-0 shadow-[0px_0px_30px_10px_rgba(255,255,255,0.5)]"></div>
 
       <nav className="flex items-start justify-between text-center md:text-left mb-10">
         <div className="flex items-center">
